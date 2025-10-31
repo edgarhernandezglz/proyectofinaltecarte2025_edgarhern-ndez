e447a9c73b01de5c3f0122442ad667af7c492896
Edgar <a01664008@tec.mx>
2025-10-31 16:29:48 -0600

    Añadi funciones para el perimetro y que se añadiera a los resultados

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -10,10 +10,13 @@ perimetros = []
 
 for index, row in dataFile.iterrows():
    if row['FIGURA'] == 't':
-      area = triangulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
+      area = a_triangulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
+      perimetro = p_triangulo(int(row['MEDIDA1'])
    elif row['FIGURA'] == 'r':
-      area = rectangulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
+      area = a_rectangulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
+      perimetetro = p_rectangulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
    elif row ['FIGURA'] == 'c':
-      area = circulo(float(row['MEDIDA1']))
-   print(f"Fila{index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}, Area={area}")
+      area = a_circulo(float(row['MEDIDA1']))
+      perimetro = p_circulo(float(row['MEDIDA1']))
+   print(f"Fila{index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}, Area={area}, Perímetro = {perimetro}")
 
--- funciones.py
+++ funciones.py
@@ -1,11 +1,23 @@
-def rectangulo(x,y):
+def a_rectangulo(x,y):
 	area=int(x*y)
 	return area
 
-def triangulo(x,y):
+def p_rectangulo(x,y):
+	perimetro=int((x*2)+(y*2))
+	return perimetro
+
+def a_triangulo(x,y):
 	area = float((x*y)/2)
 	return area
 
-def circulo(x):
+def p_traingulo(x):
+	perimetro= int(x*3)
+	return perimetro
+
+def a_circulo(x):
 	area=float(3.1416*(x**2))
 	return area
+
+def p_circulo(x):
+	perimetro=float(3.1416*(x*2))
+	return perimetro
