28a859becd59852fe8a070f814c671f91bf7e6e5
Edgar <a01664008@tec.mx>
2025-10-31 12:29:01 -0600

    Pusimos un mensaje de bienvenida

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -3,9 +3,11 @@ import pandas as pd
 
 dataFile = pd.read_csv("figuras.csv")
 
+print("Procesando figuras ...\n")
 
 areas = []
 perimetros = []
 
 for index, row in dataFile.iterrows():
    print(f"Fila{index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+
