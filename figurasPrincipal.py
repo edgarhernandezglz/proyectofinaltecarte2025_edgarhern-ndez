2c578839fc53fed4767fcd5d79258c5fdce36d7f
Edgar <a01664008@tec.mx>
2025-10-31 15:55:17 -0600

    Corregí el archivo de figuras y añadi la implementación de las funciones al codigo de python

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from funciones import triangulo, rectangulo, circulo
 
 dataFile = pd.read_csv("figuras.csv")
 
@@ -9,5 +9,11 @@ areas = []
 perimetros = []
 
 for index, row in dataFile.iterrows():
-   print(f"Fila{index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+   if row['FIGURA'] == t:
+      area = triangulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
+   elif row['FIGURA'] == r:
+      area = rectangulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
+   elif row ['FIGURA'] == c:
+      area = circulo(int(row['MEDIDA1']), int(row['MEDIDA2']))
+   print(f"Fila{index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}, Area={area}")
 
